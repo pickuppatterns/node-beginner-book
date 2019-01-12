198af5553899f4ef8dcf22f744da87645a2a8f92
pickuppatterns <pickup.patterns@gmail.com>
2019-01-12 08:53:47 -0800

    adds modified code example

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-var http = require("http");
-var server = http.createServer();
+// var http = require("http");
+// var server = http.createServer();
 
-http.createServer(function (request, response) {
+// http.createServer(function (request, response) {
+//   response.writeHead(200, { "Content-Type": "text/plain" });
+//   response.write("Hello World");
+//   response.end();
+// })
+
+// server.listen(8888);
+var http = require("http");
+
+function onRequest(request, response) {
+  console.log("Request received.");
   response.writeHead(200, { "Content-Type": "text/plain" });
   response.write("Hello World");
   response.end();
-})
+}
+http.createServer(onRequest).listen(8888);
 
-server.listen(8888);
+console.log("Server has started");
