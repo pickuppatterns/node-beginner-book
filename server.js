acef4045489aa6db61482f49de5c3af4d0f8a0ab
pickuppatterns <pickup.patterns@gmail.com>
2019-01-12 09:52:10 -0800

    updates server logic coupled with router module

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var http = require('http');
 var url = require ('url')
 
 
-function start() {
+function start(route) {
    function onRequest(request, response) {
    var pathname = url.parse(request.url).pathname;
    console.log('Request for '  + pathname + ' received.');
+
+   route(pathname);
+
    response.writeHead(200, { 'Content-Type': 'text/plain' });
    response.write('Hello World');
    response.end();
@@ -14,4 +17,3 @@ http.createServer(onRequest).listen(8888);
 
 console.log('Server has started');
 }
-exports.start = start;
