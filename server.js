7ba689c4e829d545df09fa7cfa7d407537b28f51
pickuppatterns <pickup.patterns@gmail.com>
2019-01-12 08:19:03 -0800

    adds procedural & process notes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+//require the http module that ships with Node.js and makes it accessible through the variable http
 var http = require("http");
-
+//call the function createServer
+//this function returns and object and has the method named listen giving
+//the numeric port name 8888
 http.createServer(function(request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" });
   response.write("Hello World");
