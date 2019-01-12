209a809bbaf80c1ec1578a8564797868f1b603ea
pickuppatterns <pickup.patterns@gmail.com>
2019-01-12 09:36:46 -0800

    adds url module to server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-var http = require("http");
+var http = require('http');
+var url = require ('url')
+
 
 function start() {
    function onRequest(request, response) {
-   console.log("Request received.");
-   response.writeHead(200, { "Content-Type": "text/plain" });
-   response.write("Hello World");
+   var pathname = url.parse(request.url).pathname;
+   console.log('Request for '  + pathname + ' received.');
+   response.writeHead(200, { 'Content-Type': 'text/plain' });
+   response.write('Hello World');
    response.end();
 }
 http.createServer(onRequest).listen(8888);
 
-console.log("Server has started");
+console.log('Server has started');
 }
 exports.start = start;
