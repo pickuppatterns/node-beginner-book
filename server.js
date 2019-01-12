36ab1d9b344ffe5ca13e01e06ff150dad89b01b0
pickuppatterns <pickup.patterns@gmail.com>
2019-01-12 08:34:01 -0800

    adds anonymous function notes

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var http = require("http");
 //call the function createServer
 //this function returns and object and has the method named listen giving
 //the numeric port name 8888
+
 http.createServer(function(request, response) {
+  //we are passing the create server function an anonymous function
   response.writeHead(200, { "Content-Type": "text/plain" });
   response.write("Hello World");
   response.end();
   })
-  
+  //we are passing the create server function an anonymous function
+  //which could be written: http.createServer(onRequest).listen(8888);
   .listen(8888);
