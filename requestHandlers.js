354dbc3a946d9763c1f70f00f5d56650cd242da1
pickuppatterns <pickup.patterns@gmail.com>
2019-01-13 11:58:32 -0800

    updates server fucntionality to decouple incoming content

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,22 @@
-function start() {
-  console.log("Request handler 'start' was called.");
+var exec = require("child_process").exec;
   
+  function start() {
+    console.log("Request handler 'start' was called.");
+    var content = "empty";
+  
+    exec("ls -lah", function (error, stdout, stderr) {
+      content = stdout;
+    });
+ 
+   return content;
+ }
+ 
+ function upload() {
+   console.log("Request handler 'upload' was called.");
+   return "Hello Upload";
+ }
+ 
+ exports.start = start;
+ exports.upload = upload;
 
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
-  sleep(10000);
-  return "Hello Start"; 
-  }
-  function upload() {
-    console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
-  }
-  exports.start = start;
-  exports.upload = upload;
+
