5c98b9120967a0c2e85b6f18f306856746f2b27d
pickuppatterns <pickup.patterns@gmail.com>
2019-01-13 09:37:18 -0800

    add/updates router path

--- router.js
+++ router.js
@@ -1,5 +1,4 @@
-function route (pathname) {
-  console.log('About to route a request for'  + pathname); {
-  };
-  exports.route = route;
+function route(pathname) {
+  console.log('About to route a request for' + pathname);
 }
+exports.router = route;
