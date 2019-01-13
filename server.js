6cac025fdc69e4e2418b78ea3845eb6d4829d4af
pickuppatterns <pickup.patterns@gmail.com>
2019-01-13 11:11:10 -0800

    updates router js for nonblocking failing case setup

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-var http = require('http');
-var url = require('url')
-
- 
+var http = require("http");
+var url = require("url");
+  
 function start(route, handle) {
-   function onRequest(request, response) {
-   var pathname = url.parse(request.url).pathname;
-   console.log('Request for '  + pathname + ' received.');  
-   
-   route(handle, pathname); 
-
-   response.writeHead(200, { 'Content-Type': 'text/plain' });
-   response.write('Hello World');
-   response.end();
-}
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started');
+  function onRequest(request, response) {
+  var pathname = url.parse(request.url).pathname;
+  
+  console.log("Request for " + pathname + " received.");
+  
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  var content = route(handle, pathname)
+  response.write(content);
+  response.end();
+   }
+ 
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 exports.start = start;
