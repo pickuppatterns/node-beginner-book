7b0f9a7547312c794b12bf1661e50b4b43176e98
pickuppatterns <pickup.patterns@gmail.com>
2019-01-13 10:30:05 -0800

    updates router.js and adds notes

--- router.js
+++ router.js
@@ -1,8 +1,17 @@
-function route(pathname) {
-  console.log('About to route a request for' + pathname);
-}
-//“Right now, the routing “ends” in the router, and the router is not the place to actually 
-//“do” something with the requests, because that wouldn’t scale well once our application 
-//becomes more complex.”
+function route(handle, pathname) {
+  // What we do here is, we check if a request handler for the 
+  // given pathname exists, and if it does, we simply call the 
+  // according function. Because we can access our request handler 
+  // functions from our object just as we would access an element of 
+  // an associative array, we have this nice fluent handle[pathname](); 
+  // expression I talked about earlier: “Please, handle this pathname”.
 
-exports.router = route;
+  console.log('About to route a request for' + pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname]();
+  }else{
+    console.log('No request handler found for' + pathname);
+  }
+}
+exports.route = route;
+
