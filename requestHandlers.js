5bd2c2acd822314c2e40f49c1dd1cc308d8840a5
pickuppatterns <pickup.patterns@gmail.com>
2019-01-13 13:26:45 -0800

    updates server to accomodate listeners & callbacks

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,7 +3,17 @@ var exec = require("child_process").exec;
   function start(response) {
     console.log("Request handler 'start' was called.");
     
-    var body = '<html>'+'<head>'+'<meta http-equiv="Content-Type" content="text/html; '+'charset=UTF-8" />'+'</head>'+'<body>'+'<form action="/upload" method="post">'+'<textarea name="text" rows="20" cols="60"></textarea>'+'<input type="submit" value="Submit text" />'+'</form>'+'</body>'+'</html>';
+    var body = '<html>'+'<head>'+
+                '<meta http-equiv="Content-Type" content="text/html; '+
+                'charset=UTF-8" />'+
+                '</head>'+
+                '<body>'+
+                '<form action="/upload" method="post">'+
+                '<textarea name="text" rows="20" cols="60"></textarea>'+
+                '<input type="submit" value="Submit text" />'+
+                '</form>'+
+                '</body>'+
+                '</html>';
  
     //exec("ls -lah", function (error, stdout, stderr) {
       response.writeHead(200, {"Content-Type": "text/html" });
