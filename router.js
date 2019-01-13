d726007173a2adf6918a8143f12f3c7acbb59780
pickuppatterns <pickup.patterns@gmail.com>
2019-01-13 11:06:06 -0800

    updates router js using return for test case scenario

--- router.js
+++ router.js
@@ -1,14 +1,11 @@
 function route(handle, pathname) {
   console.log('About to route a request for' + pathname);
   if (typeof handle[pathname] === 'function') {
-    //example of what NOT TO DO!!!!!!!!!!!!!!!!
     return handle[pathname]();
   }else{
     console.log('No request handler found for' + pathname);
-    return "404 Not Found";
+    return "404 NOT FOUND";
   }
 }
- //explanation: Do not use a return like you would in Ruby or php
-//running this git iteration will show why
 exports.route = route;
 
